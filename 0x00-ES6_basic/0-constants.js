093cb476fb28cc87e192910e8b64434a92cb204c
= <=>
2024-09-30 17:45:37 +0000

    build(0x00): add task 0 solution

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/**
+ * A Test documentation
+ */
+
+// task first function
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+
+// getlast function
+export function getLast() {
+  return ' is okay';
+}
+
+// taskNext function
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
