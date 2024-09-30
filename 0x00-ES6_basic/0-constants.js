834967734f63d2fb7ddfbc6cf3323df33e9df70c
= <=>
2024-09-30 18:58:57 +0000

    vc(backend): add gitignore

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,19 @@
+/**
+ * A basic use of const and let for initializations
+ */
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
