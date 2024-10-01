5f352100da8a700c56397bbb3eeba0e317e5cdea
= <=>
2024-10-01 01:01:20 +0000

    style(0x00:3): adjust file line count

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,21 +1,3 @@
-/**
- * Condenses the internals of the following function to 1 line - without
- * changing the name of each function/variable.
- *
- * export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
- * if (expansion1989 === undefined) {
- *   expansion1989 = 89;
- * }
- *
- * if (expansion2019 === undefined) {
- *   expansion2019 = 19;
- * }
- * return initialNumber + expansion1989 + expansion2019;
- * }
- */
-
-export default function getSumOfHoods(
-  initialNumber, expansion1989 = 89, expansion2019 = 19,
-) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
