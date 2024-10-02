da977110ad8436f2515057b6e81b03c40b8a63a5
= <=>
2024-10-02 20:05:01 +0000

    fix(0x01:2): attempt resolving checker fail -1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' })).catch(() => (new Error())).finally(() => {
-    console.log('Got a response from the API');
-  });
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
