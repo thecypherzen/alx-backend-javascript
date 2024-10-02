21599910011eb18e3f3387bf933df2a0f3ebc5f8
= <=>
2024-10-02 17:22:21 +0000

    style(0x01:1): resolve eslint error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject('The fake API is not working currently');
+      reject('The fake API is not working currently'); // eslint-disable-line prefer-promise-reject-errors
     }
   });
 }
