bfa3276910bef3e634d68e398006fc20a95c4e97
= <=>
2024-10-03 12:03:59 +0000

    style(0x01:7): remove docstring

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,10 +1,3 @@
-/**
- * Resolves promises on a FCFS order
- *
- * @chinaDownload{promise<object>} - a promise
- * @uSDownload{promise<object>} - a promise
- * @returns{any} - the data from resolved promise
- */
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.any([chinaDownload, USDownload])
     .then((resolved) => resolved);
