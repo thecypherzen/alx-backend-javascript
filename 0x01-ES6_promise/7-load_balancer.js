21bded9e43966d604d0e0108e92159ac079d7545
= <=>
2024-10-03 12:02:10 +0000

    build(0x01:7): add task solution and test

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,11 @@
+/**
+ * Resolves promises on a FCFS order
+ *
+ * @chinaDownload{promise<object>} - a promise
+ * @uSDownload{promise<object>} - a promise
+ * @returns{any} - the data from resolved promise
+ */
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.any([chinaDownload, USDownload])
+    .then((resolved) => resolved);
+}
