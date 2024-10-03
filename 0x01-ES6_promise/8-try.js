8d9fd327ed91c91e27de649a6e1a94533ec68ed2
= <=>
2024-10-03 12:43:58 +0000

    build(0x01:8): add task solution

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+/**
+ * Divides two numbers
+ * @numerator{number} - the numerator
+ * @numerator{number} - the denominator
+ * returns - {float} on success {error<object>} on error
+ */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('annot divide by 0');
+  }
+  return (numerator / denominator);
+}
