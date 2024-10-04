b363f583c7486d91c0b91ff84cf4e0e456cda734
= <=>
2024-10-04 00:26:54 +0000

    fix(0x01:100): return promise this time around

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
+/**
+ * Handles two promises gracefully
+ *
+ * @returns{object} - an object of promises' data on success of all
+ *                  - an object of null values on failure of any
+ */
 export default async function asyncUploadUser() {
-  const photoRes = await uploadPhoto();
-  const userRes = await createUser();
-  if (!photoRes || !userRes) {
-    return ({ photo: null, user: null });
-  }
-  return ({ photo: photoRes, user: userRes });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => ({ photo: res[0], user: res[1] }))
+    .catch(() => ({ photo: null, user: null }));
 }
