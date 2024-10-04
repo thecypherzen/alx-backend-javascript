b10aa72f22f29674d5a8528961c95c3b703da641
= <=>
2024-10-04 01:00:20 +0000

    build(0x02:1): add task solution

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return ([new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]);
+}
+
+console.log(initializeRooms());
