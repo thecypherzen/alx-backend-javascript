3030a025237e1e3502383d8c909772ba5fe34328
= <=>
2024-10-04 00:49:52 +0000

    build(0x02:0): add task solution

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+/**
+ * Represents a classroom
+ * @param {int} maxStudentsSize - max class size
+ */
+
+export default class ClassRoom{
+  constructor(maxStudentsSize){
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
