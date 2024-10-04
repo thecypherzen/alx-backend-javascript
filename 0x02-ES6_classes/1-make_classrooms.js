d54063fa5e011ac40f7db4746d744d76f9d067fa
= <=>
2024-10-04 01:03:34 +0000

    docs(0x02:1): add function docstring

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,9 @@
 import ClassRoom from './0-classroom';
 
+/**
+ * Initializes 3 ClassRoom instances
+ * @returns {array} - an array of the ClassRoom instances
+ */
 export default function initializeRooms() {
   return ([new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]);
 }
-
-console.log(initializeRooms());
