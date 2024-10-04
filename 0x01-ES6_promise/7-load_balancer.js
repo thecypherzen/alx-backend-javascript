d24692e64562eb5e55e8c15e74b30d9d2ab69e9e
= <=>
2024-10-04 00:36:15 +0000

    refactor(0x01:7): promise race method instead

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,10 @@
+/**
+ * Resolves promises on a FCFS order
+ *
+ * @chinaDownload{promise<object>} - a promise
+ * @uSDownload{promise<object>} - a promise
+ * @returns{any} - the data from resolved promise
+ */
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload])
-    .then((resolved) => resolved);
+  return Promise.race([chinaDownload, USDownload]);
 }
