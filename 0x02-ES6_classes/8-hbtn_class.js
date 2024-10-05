43ba38202bc6eb4ac78a6be9c37f1c243d720b6d
= <=>
2024-10-05 02:40:57 +0000

    build(0x02:8): add task solution

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,28 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    /* eslint no-underscore-dangle: ["error", {"allow": ["_size", "_location"]}] */
+    this._size = size;
+    this._location = location;
+  }
+
+  /* can use these to override string/number coersion of class
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+  */
+
+  /* using symbols to achieve the same thing */
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this.toString();
+  }
+}
