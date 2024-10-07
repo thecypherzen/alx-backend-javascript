5aa4bd230e1b8ab25d67aff1e5accf343006a7f3
= <=>
2024-10-07 20:41:21 +0000

    build(0x03:6): add task solution

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a set from an array
+ * @param { Object[] } array - the array to create set from
+ * @returns { Object () } - a set with unique values from array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
