551ddb4265c154d40b508ceac6580f8e71244e06
= <=>
2024-10-07 21:41:26 +0000

    build(0x03:9): add task solution

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Creates a simple map data structure
+ * @returns - the Map
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
