67df00b6db42699a71343ee685eeef59be6b2461
= <=>
2024-10-07 06:32:09 +0000

    refactor(0x02:7): use symbol to define stringTag

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -10,9 +10,15 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
-
+  /* one method of customizing the tag of out class
   [Symbol.for('nodejs.util.inspect.custom')]() {
     return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+  */
+
+  /* the second way of doing so */
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 
   toString() {
