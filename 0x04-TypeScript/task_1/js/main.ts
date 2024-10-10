25962446988468e574e207b33e41ba4b5f32911b
= <=>
2024-10-10 15:47:32 +0000

    build(0x04:1): add task solution and dependencies

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+/* define interface teacher interface */
+export default interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
