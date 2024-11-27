9ff26252fbc8e9d82f52a47ae0d5e6f864fe66c7
= <=>
2024-11-27 00:09:33 +0000

    build(0x05:7): add 7-http_express server

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,32 @@
+/**
+ * A slightly more advanced http server using express
+ */
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  let resBody = '';
+  const oldLog = console.log;
+  console.log = (...arg) => {
+    resBody += `${arg}\n`;
+  };
+  countStudents(process.argv[2])
+    .then(() => {
+      res.send(`This is the list of our students\n${resBody}`);
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+    .finally(() => {
+      console.log = oldLog;
+    });
+});
+
+app.listen(1245, 'localhost');
+
+module.exports = app;
