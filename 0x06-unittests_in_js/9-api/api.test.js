0b0d55eca5e2682a2627b4728dd8995989308959
= <=>
2024-11-30 18:31:30 +0000

    test(0x06:9-api): add module api test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,49 +2,102 @@ const http = require('http');
 const { assert } = require('chai');
 
 /* eslint-disable */
-describe('test api if successful', () => {
-  let response;
-  let resData = '';
-  let error = false;
-
-  before((done) => {
-    http.get('http://localhost:7865', (res) => {
+describe('test cart page values', () => {
+  describe('test cart page with good value', () => {
+    let response;
+    let data = '';
+    let errors = {};
+    before((done) => {
+      http.get('http://localhost:7865/cart/12', (res) => {
       response = res;
       res.on('data', (chunk) => {
-        resData += chunk;
+        data += chunk;
       });
       res.on('end', () => {
         try {
-          resData = JSON.parse(resData);
+          data = JSON.parse(data.good);
         } catch (err) {
-          // do nothing here...
+          errors.isJSON = false;
         }
         finally {
           done();
         }
       })
-         .on('error', (err) => { console.log('error'); });
+         .on('error', (err) => {
+           errors.error = err;
+           done();
+         });
+      });
+    });
+    //test cases
+    it('method is get', () => {
+      assert.equal(response.req.method, 'GET');
+    });
+
+    it('request path is /cart/12', () => {
+      assert.equal(response.req.path, '/cart/12');
+    });
+
+    it('status code should be 200', () => {
+      assert.equal(response.statusCode, 200);
+    });
+
+    it('response value should be a string', () => {
+      assert.isString(data);
+    });
+    it('response value should be valid', () => {
+      assert.isOk(data);
+      assert.equal(data, 'Payment methods for cart 12');
     });
   });
 
-  //test cases
-  it('method is get', () => {
-    assert.equal(response.req.method, 'GET');
-  });
 
-  it('request path is /', () => {
-    assert.equal(response.req.path, '/');
-  });
+  describe('test cart page with good value', () => {
+    let response;
+    let data = '';
+    let errors = {};
 
-  it('status code should be 200', () => {
-    assert.equal(response.statusCode, 200);
-  });
+    before((done) => {
+      http.get('http://localhost:7865/cart/12ed', (res) => {
+      response = res;
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        try {
+          data = JSON.parse(data.good);
+        } catch (err) {
+          errors.isJSON = false;
+        }
+        finally {
+          done();
+        }
+      })
+         .on('error', (err) => {
+           errors.error = err;
+           done();
+         });
+      });
+    });
 
-  it('response value should be a string', () => {
-    assert.isString(resData);
-  });
-  it('response value should be valid', () => {
-    assert.isOk(resData);
-    assert.equal(resData, 'Welcome to the payment system');
-  });
+    //test cases
+    it('method is get', () => {
+      assert.equal(response.req.method, 'GET');
+    });
+    it('request path is /cart/12ed', () => {
+      assert.equal(response.req.path, '/cart/12ed');
+    });
+
+    it('status code should be 404', () => {
+      assert.equal(response.statusCode, 404);
+    });
+    it('response value should be a string', () => {
+      assert.isString(data);
+    });
+    it('response value should be valid', () => {
+      assert.isOk(data);
+      assert.equal(data.split('\n')[7],
+                   '<pre>Cannot GET /cart/12ed</pre>');
+    });
+ });
 });
